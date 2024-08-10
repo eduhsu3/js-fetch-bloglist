f4065e3ddf5afc299eb3ed72bac65d2846581b1c
sungunghwang <eduhsu3@gmail.com>
2024-08-10 14:05:38 +0900

    데이터 가져오기 함수 전체 수정

--- script.js
+++ script.js
@@ -1,33 +1,61 @@
-let posts = [];
-const API_URL = 'https://jsonplaceholder.typicode.com/posts';
-//데이터 가져오기
-fetch(API_URL)
-    .then((res) => {
-        if (res.ok) {
-            return res.json();
-        } else {
-            return Promise.reject('데이터를 가져올수없습니다.');
-        }
-    })
-    .then((data) => {
-        //성공 후 실행 코드 및 함수
-        //posts = data;
-        posts = [...data];
-        renderPost(posts);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+//=======================================================================================
+document.addEventListener('DOMContentLoaded', () => {
+    const API_URL = 'https://jsonplaceholder.typicode.com'; //1페지이 20개 요청
+    //const API_URL = 'https://jsonplaceholder.typicode.com/posts?_page=1&_limit=20'; //1페지이 20개 요청
+    //const API_URL = 'https://jsonplaceholder.typicode.com/posts?title_like=sun'; //검색어 요청
 
-function renderPost(arrData) {
-    const elePostList = document.querySelector('.post_list');
-    elePostList.innerHTML = '';
+    let currentPage = 1;
+    const limit = 10;
 
-    for (let item of arrData) {
-        const { title, body } = item;
-        elePostList.innerHTML += `
-        <!-- item[s] -->
-        <div class="item">
+    function searchLoadPost(prmKeyword) {
+        fetch(`${API_URL}/posts?title_like=${prmKeyword}`)
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    return Promise.reject('데이터를 로딩중 장애발생');
+                }
+            })
+            .then((data) => {
+                document.querySelector('.post_container').innerHTML = '';
+                renderList(data);
+                console.log('검색결과', data);
+            })
+            .catch((err) => {
+                console.error('ERROR : ', err);
+            });
+    }
+
+    function loadPost(pageNum) {
+        fetch(`${API_URL}/posts?_page=${pageNum}&_limit=${limit}`)
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    return Promise.reject('데이터를 로딩중 장애발생');
+                }
+            })
+            .then((data) => {
+                //console.log(data);
+                renderList(data);
+            })
+            .catch((err) => {
+                console.error('ERROR : ', err);
+            });
+    }
+    //초기로드
+    loadPost(currentPage);
+
+    function renderList(prmData) {
+        const elePostContainer = document.querySelector('.post_container');
+
+        prmData.forEach((item, idx) => {
+            const { title, body } = item;
+
+            const eleItem = document.createElement('div');
+            eleItem.classList.add('item');
+
+            eleItem.innerHTML += `
             <a href="#">
                 <div class="item_top">
                     <p class="title">${title}</p>
@@ -38,25 +66,34 @@ function renderPost(arrData) {
                 <div class="date"><span>2024.10.10</span></div>
                 <div class="like"><a href="#">좋아요</a></div>
             </div>
-        </div>
-        <!-- item[e] -->
-        
-        `;
-    }
-}
-
-const eleSchKeyword = document.querySelector('#schKeyword');
-
-function onSearchHandler(e) {
-    const userInputKeyWord = e.target.value.trim().toLowerCase();
-    //console.log(userInputKeyWord);
-
-    function changeHighlight(text, query) {
-        if (!query) return text;
-        const regex = new RegExp(`(${query})`, 'gi');
-        return text.replace(regex, '<mark>$1</mark>');
+           `;
+            elePostContainer.appendChild(eleItem);
+        });
     }
 
+    //=======================================================================================
+    const eleMoreBtn = document.querySelector('#moreBtn');
+    eleMoreBtn.addEventListener('click', () => {
+        currentPage++;
+        loadPost(currentPage);
+    });
+
+    //=======================================================================================
+    const eleSchBtn = document.querySelector('#schBtn');
+    const eleSchInput = document.querySelector('#schKeyword');
+
+    function onSearchHandler(e) {
+        const userInputKeyWord = eleSchInput.value.trim();
+        //console.log(userInputKeyWord);
+
+        searchLoadPost(userInputKeyWord);
+
+        function changeHighlight(text, query) {
+            if (!query) return text;
+            const regex = new RegExp(`(${query})`, 'gi');
+            return text.replace(regex, '<mark>$1</mark>');
+        }
+        /* 
     const filteredData = posts
         .filter((item) => {
             const changeTitleLower = item.title.toLowerCase();
@@ -68,8 +105,9 @@ function onSearchHandler(e) {
                 ...item,
                 title: changeHighlight(item.title, userInputKeyWord),
             };
-        });
+        }); */
 
-    renderPost(filteredData);
-}
-eleSchKeyword.addEventListener('keyup', onSearchHandler);
+        //renderPost(filteredData);
+    }
+    eleSchBtn.addEventListener('click', onSearchHandler);
+});
